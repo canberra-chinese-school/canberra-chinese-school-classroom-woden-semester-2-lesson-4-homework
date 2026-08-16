47e3f46c314d3a52ff699901771db93c2f48fa01
stevengao-ytc <stevengao213@gmail.com>
2026-08-16 12:52:31 +1000

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,8 +1,9 @@
-# In class, we have made a simple turtle program that draws a square.
-# The python file (draw_square.py) for this program is included in the repository for your reference.
-# So the homework is to make a triangle in python turtle module, using the functions we learned in class.
+# In class, we have learned to draw shape frames, and colour them using begin_fill and end_fill.
+# At the end of the lesson, we briefly discussed about drawing a flower-like shape on turtle with randomised petal colours.
+# TODO: in this homework, make a nice looking flower with randomised petal colours in python turtle. An example I did is included at the end of the lesson slides.
+# This is a creative task and there isn't a fixed answer. Everyone should have their unique solutions.
+# Your flower doesn't have to be decent looking at all, as long as it is original.
 
-# TODO: edit this file to make a triangle of any colour or size you like
 # Since we use a new module called turtle, regular python compiler websites (like online-python.com) will not work.
 # Please test your script in turtle supported online python environments, like
 # "https://pythonsandbox.com/turtle"
